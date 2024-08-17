988d34e342374db82b5b0519690ad62042a90857
agwlprince617 <agrawalprince617@gmail.com>
2024-08-17 15:02:00 +0530

    adding custom guards

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -1,86 +1,76 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::system_program;
-use anchor_lang::solana_program::entrypoint::ProgramResult;
-
-
 
 declare_id!("MdueQEiG1zRdsjZXhazkLshBF7J9NagdNZFGBXsWY8p");
 
-// [error_code]
-// pub enum ErrorCode {
-//     #[msg("Provided topic is too long")]
-//     TopicTooLong,
-//     #[msg("Provided content too long")]
-//     ContentTooLong,
-// }
 
+//5. Implementing the logic since the context is ready
 #[program]
 pub mod solana_twitter {
     use super::*;
-
-    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> ProgramResult {
+    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> Result<()> {
         let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
         let author: &Signer = &ctx.accounts.author;
         let clock: Clock = Clock::get().unwrap();
 
-        // if topic.chars().count()>50 {
-        //     return Err(ErrorCode::TopicTooLong.into())
-        // }
+        if topic.chars().count() > 50 {
+            return Err(ErrorCode::TopicTooLong.into())
+        }
 
-        // if content.chars().count()>280 {
-        //     return Err(ErrorCode::ContentTooLong.into())
-        // }
+        if content.chars().count() > 280 {
+            return Err(ErrorCode::ContentTooLong.into())
+        }
 
         tweet.author = *author.key;
-        tweet.timestamp= clock.unix_timestamp;
-        tweet.topic=topic;
-        tweet.content=content;
+        tweet.timestamp = clock.unix_timestamp; 
+        tweet.topic = topic;
+        tweet.content = content;
+
         Ok(())
     }
 }
-
-// #[derive(Accounts)]
-// pub struct Initialize {}
-
-
-//Define the context {all the accounts that are necessary for the instruction to do the job}
+ // 4. Defining all the necessary context before sending the instruction
+ // Also we need to define the necessary constraints 
 #[derive(Accounts)]
 pub struct SendTweet<'info> {
-    #[account(init, payer=author, space=Tweet::LEN)]
+    #[account(init, payer = author, space = Tweet::LEN)]
     pub tweet: Account<'info, Tweet>,
     #[account(mut)]
-    pub author: Signer<'info>,  
-    #[account(address = system_program::ID)]
-    pub system_program: Program<'info,System>,
+    pub author: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
-//1. Define the structure of tweet account
+// 1. Define the tweet account
 #[account]
-pub struct Tweet{
+pub struct Tweet {
     pub author: Pubkey,
     pub timestamp: i64,
-    pub topic:String,
-    pub content: String, 
+    pub topic: String,
+    pub content: String,
 }
 
-
-//2. Adding useful constants for sizing properties
+// 2. Add some constants for sizing properties
 const DISCRIMINATOR_LENGTH: usize = 8;
 const PUBLIC_KEY_LENGTH: usize = 32;
 const TIMESTAMP_LENGTH: usize = 8;
-const STRING_LENGHT_PREFIX: usize = 4;
-const MAX_TOPIC_LENGHT: usize = 50*4;
-const MAX_CONTENT_LENGTH: usize = 280*4;
+const STRING_LENGTH_PREFIX: usize = 4; // Stores the size of the string.
+const MAX_TOPIC_LENGTH: usize = 50 * 4; // 50 chars max.
+const MAX_CONTENT_LENGTH: usize = 280 * 4; // 280 chars max.
 
 
-//3. Add a constant in the tweet that provides the total size 
+// 3. Add a constant named LEN on the Tweet account that provides the total size.
 impl Tweet {
     const LEN: usize = DISCRIMINATOR_LENGTH
-    + PUBLIC_KEY_LENGTH
-    + TIMESTAMP_LENGTH
-    + STRING_LENGHT_PREFIX + MAX_TOPIC_LENGHT
-    + STRING_LENGHT_PREFIX + MAX_CONTENT_LENGTH;
+        + PUBLIC_KEY_LENGTH // Author.
+        + TIMESTAMP_LENGTH // Timestamp.
+        + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH // Topic.
+        + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH; // Content.
 }
 
-
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The provided topic should be 50 characters long maximum.")]
+    TopicTooLong,
+    #[msg("The provided content should be 280 characters long maximum.")]
+    ContentTooLong,
+} 
 
--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -22,11 +22,11 @@ describe("solana-twitter", () => {
 
       //Before sending the transacions to the blockchain
       const tweet = anchor.web3.Keypair.generate();
-      await program.rpc.sendTweet('','Do, you know Solana?', {
+      await program.rpc.sendTweet('Solana','Do, you know Solana?', {
         accounts: {
           //Accounts here
           tweet: tweet.publicKey,
-          author: program.provider.wallet.publicKey ,
+          author: program.provider.wallet.publicKey,
           systemProgram: anchor.web3.SystemProgram.programId,
         },
         signers: [tweet],
@@ -35,7 +35,7 @@ describe("solana-twitter", () => {
       //Fetch the account details of the created tweet
       const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
       assert.equal(tweetAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58());
-      assert.equal(tweetAccount.topic, '');
+      assert.equal(tweetAccount.topic, 'Solana');
       assert.equal(tweetAccount.content, 'Do, you know Solana?');
       assert.ok(tweetAccount.timestamp);
     });
@@ -65,6 +65,55 @@ describe("solana-twitter", () => {
       assert.equal(tweetAccount.topic, '');
       assert.equal(tweetAccount.content, 'Do, you know Solana?');
       assert.ok(tweetAccount.timestamp);
+    });
+
+
+    it('can fetch all tweets', async () => {
+      const tweetAccounts=await program.account.tweet.all();
+      assert.equal(tweetAccounts.length,2)
+    });
+
+    
+    it('cannot provide a content more than 280 characters', async () => {
+
+      //Before sending the transacions to the blockchain
+      try {
+        const tweet = anchor.web3.Keypair.generate();
+        const LongContent='x'.repeat(281);
+        await program.rpc.sendTweet('Solana',LongContent, {
+          accounts: {
+            //Accounts here
+            tweet: tweet.publicKey,
+            author: program.provider.wallet.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+          },
+          signers: [tweet],
+        });
+      } catch (error) {
+        assert.equal(error.msg, 'Provided content is too long');
+      }
+      assert.fail('The instruction failed due to too long content');
+    });
+
+    it('cannot provide a topic more than 50 characters', async () => {
+
+      //Before sending the transacions to the blockchain
+      try {
+        const tweet = anchor.web3.Keypair.generate();
+        const LongTopic='x'.repeat(51);
+        await program.rpc.sendTweet(LongTopic,'Hey There', {
+          accounts: {
+            //Accounts here
+            tweet: tweet.publicKey,
+            author: program.provider.wallet.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+          },
+          signers: [tweet],
+        });
+      } catch (error) {
+        assert.equal(error.msg, 'Provided topic is too long');
+      }
+      assert.fail('The instruction failed due to too long topic');
     });
 
 });
